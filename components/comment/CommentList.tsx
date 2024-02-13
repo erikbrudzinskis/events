74f87bddd2f03515cd4a76bcb6e84d672343702e
Erik Brudzinskis <erik.brudzinskis@viskan.se>
2024-02-13 20:06:19 +0200

    Fix double className in CommentList

--- components/comment/CommentList.tsx
+++ components/comment/CommentList.tsx
@@ -1,11 +1,15 @@
 import CommentNew from "@/components/comment/CommentNew";
 import CommentItem from "@/components/comment/CommentItem";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export default function CommentList(props) {
     const [comments, setComments] = useState([]);
     const [commentVisibility, setCommentVisibility] = useState(false);
     const {eventId} = props;
+
+    useEffect(() => {
+        getComments();
+    }, [commentVisibility])
 
     function getComments() {
         fetch('/api/comment/' + eventId, {
@@ -17,7 +21,6 @@ export default function CommentList(props) {
 
     function toggleCommentVisibility() {
         setCommentVisibility(!commentVisibility);
-        getComments();
     }
 
     return (
@@ -26,7 +29,7 @@ export default function CommentList(props) {
                 <div className="row height d-flex justify-content-center align-items-center">
                     <div className="col-md-7">
                         <button className="btn btn-primary" style={{width: "12rem"}} onClick={toggleCommentVisibility}>{commentVisibility ? 'Hide' : 'Show'} Comments</button>
-                        <div className="card" className={commentVisibility ? 'visible' : 'invisible'}>
+                        <div className={commentVisibility ? 'card visible' : 'card invisible'}>
                             <div className="p-3">
                                 <h5>Comments</h5>
                             </div>
